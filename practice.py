b3f247ef1d2ba1bc0e86044cfbb65ddd45286436
ayinamubarakk <ayina.mubarak2301051@gmail.com>
2025-11-13 16:21:14 +0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,96 @@
+print("hello world")
+print("ayina")
+
+name = "Ayina"
+age = 18
+price = 20
+age2 = age
+print(age2)
+print(name)
+print(age)
+print(price)
+print("My name is",name)
+print("My age is",age)
+print("price is",price)
+
+print(type(age))
+print(type(name))
+print(type(price))
+
+name1 = "am"
+name2 = 'am'
+name3 = '''am'''
+print(name1)
+print(name2)
+print(name3)
+
+## Calculating sum,difference and division 
+
+num1 = 5
+num2 = 7
+
+sum = num1+num2
+division = num2/num2
+difference = num2-num1
+
+
+# print(int(sum))
+# print(float(division))
+# print(int(difference))
+
+print(int(sum))
+print(float(division))
+print(int(difference))
+
+a = 50
+b = 100
+print(a%b)
+
+#relational operators
+a = 20
+b = 50
+print(a==b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a!=b)
+
+#assignment operators 
+a = 20
+b = 50
+
+# = is an assignment operator and we are assigning the value 20 in the variable a
+ 
+num1 = 20
+num1 += 20
+print("num1:" ,num1)
+num1 = 20
+num1 -= 20
+print("num1:" ,num1)
+num1 = 20
+num1 *= 5
+print("num1:" ,num1)
+num1 = 20
+num1 /= 20
+print("num1:" ,num1)
+num1 = 20
+num1 **= 5
+print("num1:" ,num1)
+
+#Logical Operators
+#not operator
+a = 50
+b = 30
+print(not False)
+print(not True)
+print(not (a>b))
+print(not (a<b))
+
+#and operator
+num1 = 25
+num2 = 50
+print()
+
+val1 = True
+val2 = False
+print("and operator", val1 and val2)
